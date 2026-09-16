6a8393fae5033329a145ce11dc9b4df657970ddd
Sumanth Mahimaluru <sumanthmahimaluru@gmail.com>
2026-09-16 22:15:06 +0530

    Repo created

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Artifact Lab, This is Sumanth M")
+	fmt.Println("Hello Artifact Lab, This is Sumanth M, Just checking the artifact lab setup")
 }
